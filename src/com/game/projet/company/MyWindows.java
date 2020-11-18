947faab2632bf1c0e868324f94d5b2ca61a4a0f7
zohraDev <bach.zora@hotmail.fr>
2020-11-18 23:16:31 +0100

    Debut Interface Graphique brute

---
+++ src/com/game/projet/company/MyWindows.java
@@ -0,0 +1,16 @@
+package com.game.projet.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyWindows extends JFrame {
+    MyWindows(){
+        super("Bibliothèque");
+        this.setBackground(Color.green);
+        this.setVisible(true);
+        this.setDefaultCloseOperation();
+
+    }
+
+
+}
